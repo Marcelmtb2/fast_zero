ba0b67330fe5d38c1454bb42412bd112d5022cb3
Marcelmtb2 <mtb2@softex.cin.ufpe.br>
2025-04-11 16:35:47 -0300

    aula02

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message
+from http import HTTPStatus
+
+
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+# @app.get('/', response_class=HTMLResponse)
+# def read_root():
+#     return """
+#     <html>
+#       <head>
+#         <title> Nosso olá mundo!</title>
+#       </head>
+#       <body>
+#         <h1> Olá Mundo </h1>
+#       </body>
+#     </html>"""
